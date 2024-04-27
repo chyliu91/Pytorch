cda09db52e7f1976396e0cdf35c44eb2655f957c
chyliu91 <chyliu91@gmail.com>
2024-04-27 18:46:58 +0800

    update

---
+++ source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from recommonmark.parser import CommonMarkParser
+import sphinx_rtd_theme
+
+
+project = 'Pytorch'
+copyright = '2024, Chay'
+author = 'Chay'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+source_parsers = {
+  '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+extensions = ['recommonmark','sphinx_markdown_tables'] 
---
+++ source/index.rst
@@ -0,0 +1,8 @@
+.. toctree::
+   :maxdepth: 1
+
+   Pytorch 基础/index 
+
+
+
+
